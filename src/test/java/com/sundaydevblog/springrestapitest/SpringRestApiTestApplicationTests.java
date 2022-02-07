36352608ffe106a329217d81dff46b4188b0ee9f
Alberto <alberto28382@gmail.com>
2022-02-07 16:14:53 +0100

    initial commit

---
+++ src/test/java/com/sundaydevblog/springrestapitest/SpringRestApiTestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sundaydevblog.springrestapitest;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+//@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class SpringRestApiTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
